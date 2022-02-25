9b10ae33d7aaef887810dc18675439e9f0a2a24c
Luke Wood <l.wood0097@gmail.com>
2022-02-25 19:34:01 +0000

    added horizontal divider to stratagem cards

--- src/components/StratagemCard/StratagemCard.js
+++ src/components/StratagemCard/StratagemCard.js
@@ -3,12 +3,13 @@ import styles from "./StratagemCard.module.css";
 function StratagemCard(props) {
   return (
     <article className={styles["card"]} key={props.id}>
-      <p className={styles["card-header"]}>
-        <span className={styles["card-name"]}>{props.name}</span>
-        <span className={styles["card-cp"]}>{props.cp_cost}CP</span>
+      <p className={styles["header"]}>
+        <span className={styles["name"]}>{props.name}</span>
+        <span className={styles["cp"]}>{props.cp_cost}CP</span>
       </p>
-      <div className={styles["card-body"]}>
+      <div className={styles["body"]}>
         <p>{props.type}</p>
+        <hr className={styles["divider"]}/>
         <p>{props.description}</p>
       </div>
     </article>
