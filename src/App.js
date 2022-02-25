1216d2e966526ec30ab0d10fe4496d8701073c66
Luke Wood <l.wood0097@gmail.com>
2022-02-25 20:45:18 +0000

    added fixed sidebar with content scrolling

--- src/App.js
+++ src/App.js
@@ -9,7 +9,31 @@ import "./App.css";
 function App() {
   return (
     <React.Fragment>
-      <StratagemGrid stratagems={Stratagems} />
+      <div className="container">
+        <nav className="sidebar">
+          <div className="link">
+            <div className="text">Home</div>
+          </div>
+          <div className="link">
+            <div className="text">Projects</div>
+          </div>
+          <div className="link">
+            <div className="text">Art</div>
+          </div>
+          <div className="link">
+            <div className="text">Social</div>
+          </div>
+          <div className="link">
+            <div className="text">Setup</div>
+          </div>
+          <div className="link">
+            <div className="text">Help</div>
+          </div>
+        </nav>
+        <main className="content">
+            <StratagemGrid stratagems={Stratagems} />
+        </main>
+      </div>
     </React.Fragment>
   );
 }
